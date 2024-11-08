d790fe66d07ba02ec0d6517fa84e348a5c1345d4
Evgurov <evgurovv@yandex.ru>
2024-11-08 04:01:55 +0000

    inference in chat format

--- test_model.py
+++ test_model.py
@@ -9,23 +9,32 @@ from deepeval.dataset import EvaluationDataset
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "3,4"
 
-MODEL_NAME = "unsloth/Llama-3.2-3B-Instruct"
+MODEL_NAME = "RLHF-And-Friends/Llama-3.2-3B-Instruct"
 
 class EvaluatedModel:
     def __init__(self, tokenizer, model):
         self._model = model
         self._tokenizer = tokenizer
 
-    def __call__(self, input: list[str]) -> list[str]:
-        model_inputs = self._tokenizer(
-            input, return_tensors = "pt", padding=True
-        ).to("cuda")
-        generated_ids = self._model.generate(**model_inputs)
-        output = self._tokenizer.batch_decode(
-            generated_ids, skip_special_tokens=True
+    def __call__(self, chat: list[dict]) -> list[str]:
+        formatted_chat = self._tokenizer.apply_chat_template(
+            chat, tokenize=False, add_generation_prompt=True
+        )
+        inputs = self._tokenizer(
+            formatted_chat, return_tensors="pt", add_special_tokens=False
+        )
+        inputs = {
+            key: tensor.to(self._model.device) 
+            for key, tensor in inputs.items()
+        }
+        outputs = self._model.generate(
+            **inputs, max_new_tokens=512, temperature=0.1
+        )
+        decoded_output = self._tokenizer.decode(
+            outputs[0][inputs['input_ids'].size(1):], skip_special_tokens=True
         )
 
-        return output
+        return decoded_output
 
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, padding_size="left")
@@ -36,6 +45,7 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 tokenizer.pad_token = tokenizer.eos_token
 model = EvaluatedModel(tokenizer, model)
+
 
 def test():
     correctness_metric = GEval(
@@ -48,14 +58,13 @@ def test():
     )
 
     inputs = [
-        "What is 2+2?",
-        "Hi!"
+        {"role": "user", "content": "What is 2+2?"},
+        {"role": "user", "content": "What is 2/2*3 - 2?"}
     ]
     test_cases = [
         LLMTestCase(
-            input=input, 
-            actual_output=output
-        ) for input, output in zip(inputs, model(inputs))
+            input=input["content"], actual_output=model([input])
+        ) for input in inputs
     ]
 
     dataset = EvaluationDataset(test_cases=test_cases)
