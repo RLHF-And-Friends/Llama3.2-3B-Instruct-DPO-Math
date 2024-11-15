caca1bf5469d09708c62526367089293c322372b
evgurov <evgurovv@yandex.ru>
2024-11-15 04:34:11 +0300

    soft and hard preference metrics added (0.7 and 0.5 threshold)

--- test_model.py
+++ test_model.py
@@ -3,8 +3,8 @@ from deepeval.metrics import GEval
 from deepeval.dataset import EvaluationDataset
 
 
-metric = GEval(
-    name = "preference",
+hard_preference = GEval(
+    name = "Hard preference",
     evaluation_params=[
         LLMTestCaseParams.INPUT,
         LLMTestCaseParams.ACTUAL_OUTPUT,
@@ -20,6 +20,23 @@ metric = GEval(
     threshold=0.7
 )
 
+soft_preference = GEval(
+    name = "Soft preference",
+    evaluation_params=[
+        LLMTestCaseParams.INPUT,
+        LLMTestCaseParams.ACTUAL_OUTPUT,
+    ],
+    evaluation_steps=[
+        "Read the user request from the input carefully to understand the context and requirements.",
+        "Compare the responses of Model 1 and Model 2 for relevance, correctness, and clarity based on the user request.",
+        "Evaluate the mathematical correctness of any facts or calculations presented in both model responses.",
+        "Assign 0 score if the response of Model 1 is better, 5 score if responses are equally good, and 10 score if Model 2 response is better."
+    ],
+    model="gpt-4o-mini",
+    verbose_mode=True,
+    threshold=0.5
+)
+
 comp_dataset = EvaluationDataset()
 comp_dataset.add_test_cases_from_csv_file(
     file_path='comp_dataset.csv', 
@@ -28,4 +45,4 @@ comp_dataset.add_test_cases_from_csv_file(
 )
 
 def test_dataset():
-    comp_dataset.evaluate([metric])
+    comp_dataset.evaluate([hard_preference, soft_preference])
